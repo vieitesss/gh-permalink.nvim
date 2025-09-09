8328120e3397bd7f723921a52ba6e91b1984fedf
Daniel Vieites <dvieitest@gmail.com>
2025-09-09 13:51:52 +0200

    fix: removed function

--- lua/gh-permalink/core.lua
+++ lua/gh-permalink/core.lua
@@ -8,4 +8,24 @@ function M.urlencode_path(p)
     )
 end
 
+function M.get_line_range_and_mode()
+    local m = vim.fn.mode(1)
+
+    if m:match("^[vV]") or m == "\022" then
+        local lstart = vim.fn.getpos("v")[2]
+
+        local lend = vim.fn.getpos(".")[2]
+
+        if lstart > lend then
+            lstart, lend = lend, lstart
+        end
+
+        return true, lstart, lend
+    else
+        local l = vim.api.nvim_win_get_cursor(0)[1]
+
+        return false, l, l
+    end
+end
+
 return M
